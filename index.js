203ce642e57263e13c9af6b35e20acac3a95d213
abu bakar <abubakar850772@gmail.com>
2024-09-01 00:21:18 +0600

    finished verify admin apis

--- index.js
+++ index.js
@@ -42,18 +42,33 @@ async function run() {
         // middleware 
 
     const verifyToken = (req, res, next) => {
-      console.log("verify token",req.headers.authorization);
+      // console.log("verify token",req.headers.authorization);
 
       if(!req.headers.authorization){
-        return res.status(401).send({message: "forbidden access"})
+        return res.status(401).send({message: "unauthorized access"})
       }
       const token = req.headers.authorization.split(" ")[1];
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
-        if(err) return res.status(401).send({message: "forbidden access"});
-        req.decode = decode;
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err) return res.status(401).send({message: "unauthorized access"});
+        req.decoded = decoded;
         next()  
       })
       
+    }
+
+    //verify admin middleware 
+
+    const verifyAdmin = async(req, res, next) => {
+
+      const email = req.decoded.email;
+
+      const query = {email};
+      const result = await usersCollection.findOne(query)
+      const isAdmin = result?.role === 'admin';
+      if(!isAdmin) return res.status(403).send({message: "forbidden access"})
+
+        next()
+
     }
 
     app.post("/jwt", async( req, res) => {
@@ -63,21 +78,34 @@ async function run() {
     })
 
 
-    
-
-
-
 
     // user related apis
 
     //get all users
 
-    app.get("/users", verifyToken, async(req, res) => {
+    app.get("/users", verifyToken,verifyAdmin, async(req, res) => {
       // console.log(req.headers);
       
       const result = await usersCollection.find().toArray();
       res.send(result)
     })
+
+    app.get("/users/admin/:email",verifyToken, async(req, res) => {
+
+      const email = req.params.email;
+      
+      if(email !== req.decoded.email) return res.status(403).send({message: "forbidden access"})
+      const filter = {email: email};
+      const result = await usersCollection.findOne(filter)
+
+      
+
+      let admin = false;
+      if(result){
+        admin = result?.role === 'admin'
+      }
+      res.send({admin})
+    } )
 
     // store user on database 
 
